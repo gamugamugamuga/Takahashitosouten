ee5558e0cac2485241dbf317b225ca477d876492
gamugamugamuga <kohata_shin@yahoo.co.jp>
2023-12-17 16:25:42 +0900

    Update header.php

--- app/public/wp-content/themes/takahashitosou/header.php
+++ app/public/wp-content/themes/takahashitosou/header.php
@@ -10,50 +10,40 @@
  */
 
 ?>
-<!doctype html>
+<!DOCTYPE html>
 <html <?php language_attributes(); ?>>
 <head>
-	<meta charset="<?php bloginfo( 'charset' ); ?>">
-	<meta name="viewport" content="width=device-width, initial-scale=1">
-	<link rel="profile" href="https://gmpg.org/xfn/11">
-
-	<?php wp_head(); ?>
+    <meta charset="<?php bloginfo( 'charset' ); ?>">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="profile" href="https://gmpg.org/xfn/11">
+    <?php wp_head(); ?>
 </head>
 
 <body <?php body_class(); ?>>
-<?php wp_body_open(); ?>
-<div id="page" class="site">
-	<a class="skip-link screen-reader-text" href="#primary"><?php esc_html_e( 'Skip to content', 'takahashitosou' ); ?></a>
+<header id="masthead" class="site-header" role="banner">
+    <div class="site-branding">
+        <?php
+        if ( function_exists( 'the_custom_logo' ) ) {
+            the_custom_logo();
+        }
+        ?>
+        <div class="site-title">
+            <?php if ( is_front_page() && is_home() ) : ?>
+                <h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
+            <?php else : ?>
+                <p class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></p>
+            <?php endif; ?>
+        </div>
+    </div><!-- .site-branding -->
 
-	<header id="masthead" class="site-header">
-		<div class="site-branding">
-			<?php
-			the_custom_logo();
-			if ( is_front_page() && is_home() ) :
-				?>
-				<h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
-				<?php
-			else :
-				?>
-				<p class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></p>
-				<?php
-			endif;
-			$takahashitosou_description = get_bloginfo( 'description', 'display' );
-			if ( $takahashitosou_description || is_customize_preview() ) :
-				?>
-				<p class="site-description"><?php echo $takahashitosou_description; // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped ?></p>
-			<?php endif; ?>
-		</div><!-- .site-branding -->
+    <nav id="site-navigation" class="main-navigation" role="navigation">
+        <?php
+        wp_nav_menu( array(
+            'theme_location' => 'menu-1',
+            'menu_id'        => 'primary-menu',
+        ) );
+        ?>
+    </nav><!-- #site-navigation -->
+    <!-- ここに追加のヘッダーコンテンツがあれば記述 -->
+</header><!-- #masthead -->
 
-		<nav id="site-navigation" class="main-navigation">
-			<button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false"><?php esc_html_e( 'Primary Menu', 'takahashitosou' ); ?></button>
-			<?php
-			wp_nav_menu(
-				array(
-					'theme_location' => 'menu-1',
-					'menu_id'        => 'primary-menu',
-				)
-			);
-			?>
-		</nav><!-- #site-navigation -->
-	</header><!-- #masthead -->
